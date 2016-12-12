0be83f486069ff886c83e05c172adf6a2e3fdcfe
AyzenFTW <humbertdany@gmail.com>
2016-12-12 11:03:03 +0100

    Added test for BinaryStack v2

--- src/test/java/com/humbertdany/tpproject/test/TestBinaryStack.java
+++ src/test/java/com/humbertdany/tpproject/test/TestBinaryStack.java
@@ -4,7 +4,12 @@
  */
 package com.humbertdany.tpproject.test;
 
-import com.humbertdany.tpproject.util.binarystack.*;
+import com.humbertdany.tpproject.util.binarystack.v1.BinaryStack;
+import com.humbertdany.tpproject.util.binarystack.v1.Node;
+import com.humbertdany.tpproject.util.binarystack.v2.TasBinaire;
+import com.humbertdany.tpproject.util.factory.ArrayFactory;
+
+import java.util.Arrays;
 
 /**
  *
@@ -15,15 +20,26 @@ public class TestBinaryStack extends ATest {
 	public TestBinaryStack(){
 		
 	}
+
+	final private ArrayFactory<N> nArrayFactory = new ArrayFactory<N>() {
+		@Override
+		public N[] buildArray(int dimension) {
+			return new N[dimension];
+		}
+	};
+
+
 	
 	final public void launch(){
+
+		// Test v1
 		final N root = new N(1);
 		final N n1 = new N(50);
 		final N n2 = new N(4);
 		final N n3 = new N(2);
 		final N n4 = new N(5);
 
-		final BinaryStack<N> l1 = new BinaryStack<>(root);
+		final BinaryStack<N> l1 = new BinaryStack<>(nArrayFactory, root);
 		l1.add(root, n1, BinaryStack.ORIENTATION_LEFT);
 		l1.add(root, n2, BinaryStack.ORIENTATION_RIGHT);
 		l1.add(n2, n3, BinaryStack.ORIENTATION_LEFT);
@@ -35,6 +51,34 @@ public class TestBinaryStack extends ATest {
 		
 		l1.printInOrder();
 		l1.printPostOrder();
+
+		//TODO Very Important : fix Mitsu class
+
+		// Test v2
+		final TasBinaire<String> h = new TasBinaire<>(new ArrayFactory<String>() {
+			@Override
+			public String[] buildArray(int dimension) {
+				return new String[dimension];
+			}
+		});
+
+		h.insert("p");
+		h.insert("r");
+		h.insert("i");
+		h.insert("o");
+		System.out.println(h);
+		h.deleteMin();
+		System.out.println(h);
+
+		TasBinaire<Integer> tmp = new TasBinaire<>(new ArrayFactory<Integer>() {
+			@Override
+			public Integer[] buildArray(int dimension) {
+				return new Integer[dimension];
+			}
+		});
+		Integer[] a = {4, 7, 7, 7, 5, 0, 2, 3, 5, 1};
+		tmp.heapSort(a);
+		System.out.println(Arrays.toString(a));
 	}
 	
 	private class N extends Node<Integer> {
